f91a73f3df56cbd9cfba6f524fb0aafcbda022c5
Habibsdr <khansdr5@gmail.com>
2023-01-06 15:58:05 +0530

    Adding Slider

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Menu from './Component/Menu';
+import Slider from './Component/Slider';
 
 function App() {
   return (
     <>
     <Menu />
+    <Slider/>
     <h1>Hello World !</h1>
     </>
     
